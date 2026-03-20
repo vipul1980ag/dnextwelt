19954e7f795181c846999d3eb6e62804e13f3144
Vipul Agrawal <poonam.singhal@dnextwelt.com>
2026-03-20 15:23:38 +0100

    Fix wixLocation import in masterPage.js

--- src/pages/masterPage.js
+++ src/pages/masterPage.js
@@ -1,6 +1,8 @@
 // API Reference: https://www.wix.com/velo/reference/api-overview/introduction
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
 
+import wixLocation from 'wix-location';
+
 $w.onReady(function () {
     // Handle navigation messages from the HTML nav component
     $w('#html1').on('message', (event) => {
