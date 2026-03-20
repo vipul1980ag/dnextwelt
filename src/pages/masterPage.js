5386e42c50a46624affb102b3aa7f707fb811fae
Vipul Agrawal <poonam.singhal@dnextwelt.com>
2026-03-20 16:35:13 +0100

    Add anchor links to nav and scroll handling in masterPage

--- src/pages/masterPage.js
+++ src/pages/masterPage.js
@@ -2,12 +2,21 @@
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
 
 import wixLocation from 'wix-location';
+import wixWindow from 'wix-window';
 
 $w.onReady(function () {
     // Handle navigation messages from the HTML nav component
     $w('#html1').on('message', (event) => {
         if (event.data && event.data.href) {
-            wixLocation.to(event.data.href);
+            const href = event.data.href;
+            if (href.startsWith('#')) {
+                // Anchor link - scroll to section on same page
+                const anchorId = href.substring(1);
+                $w('#' + anchorId).scrollTo();
+            } else {
+                // Regular page link - navigate to page
+                wixLocation.to(href);
+            }
         }
     });
 });
